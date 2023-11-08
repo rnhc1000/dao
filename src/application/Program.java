268f327ab778f603a92798b18de0b939da195cf9
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-11-08 17:45:48 -0300

    feat(DAO) insert seller <-> sellerDaoJDBC.insert(Seller o)

--- src/application/Program.java
+++ src/application/Program.java
@@ -12,12 +12,17 @@ public class Program {
   public static void main(String[] args) {
     SellerDao sellerDao = DaoFactory.createSellerDao();
     Department department = new Department(2, null);
-    System.out.println("+++++++ Test One: seller findById +++++++");
-    Seller seller = sellerDao.findById(4);
-    System.out.println("+++++++ Test Two: seller findByDepartment +++++++");
-    List<Seller> list = sellerDao.findByDepartment(department);
-    list.forEach(System.out::println);
-    System.out.println("+++++++ Test Three: seller findAll +++++++");
+//    System.out.println("+++++++ Test One: seller findById +++++++");
+//    Seller seller = sellerDao.findById(4);
+//    System.out.println("+++++++ Test Two: seller findByDepartment +++++++");
+//    List<Seller> list = sellerDao.findByDepartment(department);
+//    list.forEach(System.out::println);
+//    System.out.println("+++++++ Test Three: seller findAll +++++++");
+//    List<Seller> listAll = sellerDao.findAll();
+//    listAll.forEach(System.out::println);
+    System.out.println("+++++++ Test Four: seller insert +++++++");
+    Seller newSeller = new Seller(null, "Greg Madison", "greg@gmail.com", new Date(), 3999.99, department);
+    sellerDao.insert(newSeller);
     List<Seller> listAll = sellerDao.findAll();
     listAll.forEach(System.out::println);
 
