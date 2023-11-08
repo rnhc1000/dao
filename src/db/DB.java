4a89ef317cd7c53765794a84de5729ff57f5b206
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-11-08 11:44:45 -0300

    feat(DAO) getting connected to the database-> db.properties, DBException...

---
+++ src/db/DB.java
@@ -0,0 +1,58 @@
+package db;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class DB {
+
+  private static Connection connection = null;
+
+  public static Connection getConnection() {
+
+    if (connection == null) {
+      try {
+        Properties props = loadProperties();
+        String url = props.getProperty("dburl");
+        connection = DriverManager.getConnection(url, props);
+      } catch (SQLException ex) {
+        throw new DbException(ex.getMessage());
+      }
+    }
+    return connection;
+  }
+
+  private static Properties loadProperties() {
+    try (FileInputStream fs = new FileInputStream("src/db/db.properties")) {
+
+      Properties props = new Properties();
+      props.load(fs);
+      return props;
+
+    } catch (IOException e) {
+      throw new DbException(e.getMessage());
+    }
+  }
+
+  public static void closeConnection() {
+    if (connection != null) {
+      try {
+        connection.close();
+      } catch (SQLException e) {
+        throw new DbException(e.getMessage());
+      }
+    }
+  }
+
+  public static void closeStatement(Statement st) {
+    if (st != null) {
+      try {
+        st.close();
+      } catch (SQLException ex) {
+        throw new DbException(ex.getMessage());
+      }
+    }
+
+  }
+}
