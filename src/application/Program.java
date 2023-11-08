56c58148dcb664276b9b28bf72e88102e457307d
Ricardo Ferreira <rnhc1000@hotmail.com>
2023-11-08 12:00:06 -0300

    feat(DAO) - Serialized seller class created

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,22 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
   public static void main(String[] args) {
-    Department obj = new Department(1, "Books");
-    System.out.println(obj);
+    Department department = new Department(1, "Books");
+    System.out.println(department);
+    Seller seller = new Seller(
+            21,
+            "Bob",
+            "bob@gmail",
+            new Date(),
+            3000.00,
+            department
+    );
+    System.out.println(seller);
   }
 }
